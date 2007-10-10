fa966ad573c9e62b2d1f804749d705e6b8a2ebe9
Nick Hemsley <nick.hems@gmail.com>
2007-10-10 05:33:10 +0000

    Update PostgreSQLTransformationProviderTest code to add ForignKey

--- test/Providers/PostgreSQLTransformationProviderTest.cs
+++ test/Providers/PostgreSQLTransformationProviderTest.cs
@@ -22,13 +22,13 @@ namespace Migrator.Providers.Tests
 				throw new ArgumentNullException("ConnectionString", "No config file");
 
 			_provider = new PostgreSQLTransformationProvider(constr);
-			_provider.Logger = Logger.ConsoleLogger();
+			//_provider.Logger = Logger.ConsoleLogger();
 			_provider.BeginTransaction();
-			
+
 			_provider.AddTable("Test2",
-			                   new Column("Id", typeof(int), ColumnProperties.PrimaryKeyWithIdentity),
-								new Column("TestId", typeof(int))
-									);
+								new Column("Id", typeof(int), ColumnProperties.PrimaryKeyWithIdentity),
+								new Column("TestId", typeof(int), ColumnProperties.ForeignKey)
+							);
 		}
 
 	}
