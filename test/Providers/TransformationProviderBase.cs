d972ec772a11e7baa583b850770500c7180ee083
Nick Hemsley <nick.hems@gmail.com>
2008-05-01 03:44:52 +0000

    Added tests for adding each column type that migrator supports.

--- test/Providers/TransformationProviderBase.cs
+++ test/Providers/TransformationProviderBase.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using Migrator.Columns;
+using System;
 
 namespace Migrator.Providers.Tests
 {
@@ -167,7 +169,141 @@ namespace Migrator.Providers.Tests
 			Assert.AreEqual(1, _provider.CurrentVersion);
 			Assert.IsTrue(_provider.TableExists("SchemaInfo"), "No SchemaInfo table created");
 		}
-		
+
+		#region Type Support Checks
+
+		[Test]
+		public void AddCharTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(char), 20);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddStringTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(string), 200);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddTextTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(string), (int)byte.MaxValue+1);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddLongTextTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(string), (int)ushort.MaxValue+1);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddIntegerTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(int));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddLongTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(long));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddFloatTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(float));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddDoubleTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(double));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddDecimalTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(decimal), 10);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddDecimalColumnTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", new DecimalColumn("TestType", 10, 2));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddBoolTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(bool));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+
+		[Test]
+		public void AddDateTimeTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(DateTime));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddDateTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", new DateColumn("TestType"));
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddBinaryTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(byte[]), 20);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddBlobTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(byte[]), (int)byte.MaxValue + 1);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		[Test]
+		public void AddLongBlobTypeToTable()
+		{
+			AddTable();
+			_provider.AddColumn("Test", "TestType", typeof(byte[]), (int)ushort.MaxValue + 1);
+			Assert.IsTrue(_provider.ColumnExists("Test", "TestType"));
+		}
+
+		#endregion
+
+
 		/// <summary>
 		/// Reproduce bug reported by Luke Melia & Daniel Berlinger :
 		/// http://macournoyer.wordpress.com/2006/10/15/migrate-nant-task/#comment-113
