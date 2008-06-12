9c2761b02e38b95c1ba2852569da148fbd1bccc7
Geoffrey Lane <geofflane@gmail.com>
2008-06-12 19:26:55 +0000

    Issue #17 Add a SqlServer2005Dialect to support varchar(MAX), nvarchar(MAX) and varbinary(MAX) which are recommended over text, ntext and image

---
+++ src/Migrator.Providers/Impl/SqlServer/SqlServer2005Dialect.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Data;
+
+namespace Migrator.Providers.SqlServer
+{
+    public class SqlServer2005Dialect : SqlServerDialect
+    {
+        public SqlServer2005Dialect() :base()
+        {
+            RegisterColumnType(DbType.AnsiString, 2147483647, "VARCHAR(MAX)");
+            RegisterColumnType(DbType.Binary, 2147483647, "VARBINARY(MAX)");
+            RegisterColumnType(DbType.String, 1073741823, "NVARCHAR(MAX)");
+            RegisterColumnType(DbType.Xml, 1073741823, "XML");
+        }
+
+        public override Type TransformationProvider { get { return typeof (SqlServerTransformationProvider); } }
+
+    }
+}
---
+++ src/Migrator.Tests/Providers/SqlServer2005TransformationProviderTest.cs
@@ -0,0 +1,41 @@
+#region License
+//The contents of this file are subject to the Mozilla Public License
+//Version 1.1 (the "License"); you may not use this file except in
+//compliance with the License. You may obtain a copy of the License at
+//http://www.mozilla.org/MPL/
+//Software distributed under the License is distributed on an "AS IS"
+//basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//License for the specific language governing rights and limitations
+//under the License.
+#endregion
+
+using System;
+using System.Configuration;
+using System.Data;
+using Migrator.Framework;
+using Migrator.Providers;
+using Migrator.Providers.SqlServer;
+using Migrator.Tests.Providers;
+using NUnit.Framework;
+
+namespace Migrator.Tests.Providers
+{
+    [TestFixture, Category("SqlServer2005")]
+    public class SqlServer2005TransformationProviderTest : TransformationProviderConstraintBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            string constr = ConfigurationManager.AppSettings["SqlServer2005ConnectionString"];
+
+            if (constr == null)
+                throw new ArgumentNullException("SqlServer2005ConnectionString", "No config file");
+
+            _provider = new SqlServerTransformationProvider(new SqlServer2005Dialect(), constr);
+            _provider.BeginTransaction();
+            _provider.AddTable("TestTwo",
+                               new Column("Id", DbType.Int32, ColumnProperty.PrimaryKeyWithIdentity),
+                               new Column("TestId", DbType.Int32));
+        }
+    }
+}
